ee34fa1dea9799fcda8282353229f0cdf6e6ed52
Yalcin <yalcin.coding@gmail.com>
2023-07-17 12:11:41 +0200

    03a. Vorbereitung -Trennen der Funktionalitäten

--- script_01.js
+++ script_01.js
@@ -13,11 +13,6 @@ function test()
     console.log("Hallo Tobias!");
 }
 
-
-
-
-
-
 /***** Funktionen 02a *****/
 // 2a. Parametrisierung + Datenübergabe von INNEN
 
@@ -30,11 +25,6 @@ function ausgabeNamen() {
 }
 
 // console.log(firstName);  // Fehler --> SCOPE!
-
-
-
-
-
 
 /***** Funktionen 02b *****/
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
@@ -49,17 +39,29 @@ function ausgabeNamen2(firstName) {
     console.log("Hallo " + firstName +"!");
 }
 
-
-
-
-
-
 /***** Funktionen 02c *****/
 // 2c. Mehrere Parameter / Argumente
 
-ausgabeNamenParams("Max","Mütze");
-ausgabeNamenParams(prompt("Vorname?"),prompt("Name?"));
+// ausgabeNamenParams("Max","Mütze");
+// ausgabeNamenParams(prompt("Vorname?"),prompt("Name?"));
 
 function ausgabeNamenParams(firstName, familyName) {
     console.log("Hallo " + firstName + " " + familyName + "!"); 
+}
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Funktionalitäten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+ausgabeNamenParams2("Max","Mütze");
+
+function ausgabeNamenParams2(firstName, familyName) {
+
+    // 1. Funktionalität: string composing
+    const GAP = " ";
+    let outputStr = "Hallo" + GAP + firstName + GAP + familyName + "!";
+    
+    // 2. Funktionalität: string output
+    console.log(outputStr); 
 }
